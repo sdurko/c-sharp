d0e9abf3ece35f612416b67210aa9dc311f37f36
sdurko <sdurko@gmail.com>
2013-11-16 12:41:21 -0600

    Struct and Reference Test App - C#

---
+++ StructAndReference/StructAndReference/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAndReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = new RandomClass();
+            rc.WorkWithValue();
+            rc.Display();
+
+            Console.Read();
+        }
+    }
+
+
+
+    public class SimpleExample
+    {
+        int value;
+        public SimpleExample(ref int variableOfWhichINeedAReference)
+        {
+            //Of course this won't work, but I'll keep it simple.
+            value = variableOfWhichINeedAReference;
+        }
+        public void DisplayValue()
+        {
+            Console.WriteLine(value.ToString(CultureInfo.InvariantCulture));
+        }
+        public void UpdateValue(int newValue)
+        {
+            value = newValue;
+        }
+    }
+    public class RandomClass
+    {
+        int myValue = 10;
+        private SimpleExample s;
+
+        public RandomClass()
+        {
+            s = new SimpleExample(ref myValue);
+        }
+        public void WorkWithValue()
+        {
+            myValue++;
+            s.UpdateValue(myValue);
+        }
+
+        public void Display()
+        {
+            s.DisplayValue();
+        }
+
+    }
+
+
+
+}
